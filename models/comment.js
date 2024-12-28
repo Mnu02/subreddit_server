659d562ad7b93ffb6abae9005b5d33393c5efe20
Mnumzana Moyo <mfm222@cornell.edu>
2024-12-28 13:48:29 -0500

    Added endpoint for user to subscribe/unsubscribe

--- models/comment.js
+++ models/comment.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const CommentSchema = new mongoose.Schema({
   body: { type: String, required: true },
   
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'post', required: true },
 }, {
   timestamps: true
 });
 
-module.exports = mongoose.model('Comment', CommentSchema);
+module.exports = mongoose.model('comment', CommentSchema);
