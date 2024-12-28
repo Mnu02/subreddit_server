2d986225e1ae6adfb2d736bd31d49df061d5615e
Mnumzana Moyo <mfm222@cornell.edu>
2024-12-28 03:13:05 -0500

    Added subreddit

---
+++ models/subreddit.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const SubredditSchema = new mongoose.Schema({
+    name : {type: String, required: true, unique: true},
+    description: {type: String}
+});
+
+module.exports = mongoose.model('Subreddit', SubredditSchema);
