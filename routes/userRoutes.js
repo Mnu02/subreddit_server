4683b786675915d7971bfce7815b3699758c0095
Mnumzana Moyo <mfm222@cornell.edu>
2024-12-30 14:02:19 -0500

    Validate required fields uid and username in POST

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,10 @@
-// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const Post = require('../models/post');
-const Upvote = require('../models/vote');
+const Vote = require('../models/vote');
 const Subreddit = require('../models/subreddit');
 
-// GET /api/users/:userId
-// Return user info, subreddits they subscribe to, total upvotes, etc.
 router.get('/:userId', async (req, res) => {
   try {
     const user = await User.findOne({ uid: req.params.userId });
@@ -15,43 +12,41 @@ router.get('/:userId', async (req, res) => {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    // Subreddits they subscribe to: 
-    // This depends on how you store subscriptions. 
-    // If you store them in the User object, you can just return them from the user doc.
-    // For example:
-    // const subscribedSubreddits = user.subscribedSubreddits;
-
-    // Total upvotes: we can compute by checking how many Upvotes the user’s posts have:
     const userPosts = await Post.find({ userId: user._id });
     const postIds = userPosts.map(p => p._id);
-    const upvoteCount = await Upvote.countDocuments({ postId: { $in: postIds } });
+    const upvoteCount = await Vote.countDocuments({ postId: { $in: postIds } });
     
-    // Construct a response
     res.json({
-      id: user.uid,
+      uid: user.uid,
       username: user.username,
-      // subscribedSubreddits: user.subscribedSubreddits || [],
       totalUpvotesReceived: upvoteCount,
+      subscribedReddits: user.subscribedReddits
     });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
-// Example: POST /api/users
-// When a new user signs in via Firebase, you might want to create a corresponding doc
 router.post('/', async (req, res) => {
   try {
     const { uid, username } = req.body;
+
+    // Validate required fields
+    if (!uid || !username) {
+      return res.status(400).json({ error: 'uid and username are required' });
+    }
+
     let user = await User.findOne({ uid });
     if (!user) {
       user = new User({ uid, username });
       await user.save();
     }
+
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
+
 
 module.exports = router;
