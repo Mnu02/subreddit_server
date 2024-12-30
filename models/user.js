b3ca6d5d33a7eb83bc79b2b013c7c1efc4d287d2
Mnumzana Moyo <mfm222@cornell.edu>
2024-12-30 13:58:19 -0500

    Changed from 'id' to 'uid'

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    id: {type: String, required: true, unique: true},
-    username: {type: String, required: true},
-    subscribedReddits: [{type: mongoose.Schema.Types.ObjectId, ref: 'subreddit'}]
-} , {
-    timestamp : true
+    uid: { type: String, required: true, unique: true }, // Changed from 'id' to 'uid'
+    username: { type: String, required: true },
+    subscribedReddits: [{ type: mongoose.Schema.Types.ObjectId, ref: 'subreddit' }]
+}, {
+    timestamps: true // Fixed typo
 });
 
-module.exports = mongoose.model('user', UserSchema)
+module.exports = mongoose.model('user', UserSchema);
